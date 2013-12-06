48c616d93ada22d8065415ae735ec0e2fac2dd4e
zhangying05 <zhangying3210@gmail.com>
2013-12-06 13:54:13 +0800

    【feature】添加light config 命令

---
+++ action/config.js
@@ -0,0 +1,19 @@
+var light = require('../light.js');
+var client = light.client;
+
+exports.name = 'config';
+exports.desc = 'edit config of light';
+exports.usage = [
+    '',
+    '',
+    '   light config set <key> <value>',
+    '   light config get [<key>]',
+    '   light config ls',
+    ''
+].join('\n');
+
+exports.register = function(commander){
+    commander.action(function(){
+
+    });
+};
